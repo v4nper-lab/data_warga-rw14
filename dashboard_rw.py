4183e49fb96a540333afae90fd8029db8c748c5d
v4nper-lab <v4nper@gmail.com>
2026-08-02 16:28:43 +0700

    Add files via upload

--- dashboard_rw.py
+++ dashboard_rw.py
@@ -637,7 +637,7 @@ with tab5:
 
         kolom_hub = next((c for c in df.columns if "HUBUNGAN" in c or "STATUS KELUARGA" in c or "KEDUDUKAN" in c), None)
 
-        kata_kunci = st.text_input("🔎 Ketik Nama Warga / Kata Depan (Bebas Huruf Besar/Kecil, misal: agus, aan, irvan):", key="input_pencarian_stabil_v16")
+        kata_kunci = st.text_input("🔎 Ketik Nama Warga / Kata Depan (Bebas Huruf Besar/Kecil, misal: agus, aan, irvan):", key="input_pencarian_stabil_v17")
         
         if kata_kunci:
             kw = str(kata_kunci).strip().lower()
